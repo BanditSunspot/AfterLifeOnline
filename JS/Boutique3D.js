114b4be7f88bcbc22743ee9a477ba0db97582573
BanditSunspot <65348913+BanditSunspot@users.noreply.github.com>
2024-07-07 19:54:09 +0200

    Small Update
    - Footers now contains Disord Community link
    - Boutique page now have a footer
    - Boutique page small issue with 3D object fix
    - Team page have now a footer

--- JS/Boutique3D.js
+++ JS/Boutique3D.js
@@ -12,7 +12,7 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-document.body.appendChild(renderer.domElement);
+document.getElementById("ModelView").appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
@@ -61,8 +61,6 @@ loader.load('scene.gltf', (gltf) => {
 
   mesh.position.set(0, 1.05, -1);
   scene.add(mesh);
-
-  document.getElementById('progress-container').style.display = 'none';
 }, (xhr) => {
   console.log(`loading ${xhr.loaded / xhr.total * 100}%`);
 }, (error) => {
